dccb6cae25e9ad983722e11ace28132375820974
Arthur Schumacher <arthur.s.cunha00@gmail.com>
2024-04-16 07:08:32 -0400

    feat: Added profile picture o user sign up

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,21 +1,39 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { PrismaService } from 'src/prisma.service';
 import * as bcrypt from 'bcrypt';
+import { SupabaseService } from 'src/supabase.service';
 
 @Injectable()
 export class UserService {
-  constructor(private prisma: PrismaService) {}
+  constructor(
+    private prisma: PrismaService,
+    private supabase: SupabaseService,
+  ) {}
 
-  async create(createUserDto: CreateUserDto) {
-    const salt = await bcrypt.genSalt();
+  async create(createUserDto: CreateUserDto, image: Express.Multer.File) {
+    try {
+      const { data } = await this.supabase
+        .getSupabaseClient()
+        .storage.from('images')
+        .upload(image.originalname, image.buffer, {
+          upsert: true,
+        });
 
-    return this.prisma.user.create({
-      data: {
-        username: createUserDto.username,
-        email: createUserDto.email,
-        password: await bcrypt.hash(createUserDto.password, salt),
-      },
-    });
+      const salt = await bcrypt.genSalt();
+
+      return this.prisma.user.create({
+        data: {
+          username: createUserDto.username,
+          email: createUserDto.email,
+          password: await bcrypt.hash(createUserDto.password, salt),
+          profile_picture: data.path,
+        },
+      });
+    } catch (error) {
+      throw new BadRequestException(
+        `Falha ao cadastrar usuário. e: ${error.message}`,
+      );
+    }
   }
 }
